2b666131921b3be71f6a8c59b0d4d133ac0db74c
Tam Vu <vutam_a@hotmail.com>
2019-07-03 19:54:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,8 @@ class Transfer
       sender.balance = sender.balance - @amount 
       receiver.balance = receiver.balance + @amount
       @status = "complete"
-    else 
+    else
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end 
   end 
