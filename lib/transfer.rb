240429d34bf4061fe86f5e5023ea35ad0131a46b
Tam Vu <vutam_a@hotmail.com>
2019-07-03 20:01:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,6 +32,7 @@ class Transfer
     if @status == "complete"
       sender.balance = sender.balance + @amount
       receiver.balance = receiver.balance - @amount
+      @status = "reversed"
     else 
     end 
   end 
