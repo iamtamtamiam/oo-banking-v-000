be6fae8719fb88d80955c2e10f088aa1672e5132
Tam Vu <vutam_a@hotmail.com>
2019-07-03 19:13:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,15 @@
 class Transfer
   
-  attr_accessor :sender, :receiver, :status, :amount
+  attr_accessor :sender, :receiver, :status, :amount, :bank_account
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
     @amount = amount
+  end 
+  
+  def valid?
+    
   end 
 end
