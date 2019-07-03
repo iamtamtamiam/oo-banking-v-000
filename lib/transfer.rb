ca29f68a79ac4b768476a38fba8fb8d869e7f289
Tam Vu <vutam_a@hotmail.com>
2019-07-03 19:41:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end 
   
   def execute_transaction(sender, receiver)
-    if valid? == true
+    if sender.valid? == true
       sender.balance = sender.balance - @amount 
       receiver.balance = receiver.balance + @amount
       @status = "complete"
